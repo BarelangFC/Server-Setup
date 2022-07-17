207f6514ad44748c56dcccb2b07ff64bc4ed295b
Eko Rudiawan Jamzuri <eko.rudiawan@gmail.com>
2022-07-17 14:52:20 +0700

    add script to setup CUDNN

--- scripts/setup-cuda.sh
+++ scripts/setup-cuda.sh
@@ -5,3 +5,7 @@ wget https://developer.download.nvidia.com/compute/cuda/11.7.0/local_installers/
 sudo dpkg -i cuda-repo-ubuntu2004-11-7-local_11.7.0-515.43.04-1_amd64.deb
 sudo cp /var/cuda-repo-ubuntu2004-11-7-local/cuda-*-keyring.gpg /usr/share/keyrings/
 sudo apt update && sudo apt install -y cuda
+sudo dpkg -i cudnn-local-repo-ubuntu2004-8.4.1.50_1.0-1_amd64.deb
+sudo cp /var/cudnn-local-repo-ubuntu2004-8.4.1.50/cudnn-local-E3EC4A60-keyring.gpg /usr/share/keyrings/
+sudo apt update && sudo apt install -y \
+	libcudnn8 libcudnn8-dev libcudnn8-samples
